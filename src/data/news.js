b3ee9028d8b78f922e99e531718f14e7cffa131b
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2025-12-31 19:03:04 +0530

    Update news data

--- src/data/news.js
+++ src/data/news.js
@@ -1,17 +1,5 @@
 export const news = [
   {
-    "id": 1767187584131,
-    "title": {
-      "en": " Stone Laying Ceremony of Mega Dairy",
-      "ka": " ಡೈರಿಯ ಭೂಮಿಪೂಜೆ ಸಮಾರಂಭ"
-    },
-    "description": {
-      "en": "foundation stone",
-      "ka": "ಭೂಮಿಪೂಜೆಯನ್ನು "
-    },
-    "image": "/images/1767187584129-Screenshot-2024-11-25-122137.png"
-  },
-  {
     "id": 1,
     "title": {
       "en": "Foundation Stone Laying Ceremony of Mega Dairy",
