d61078bd2bd94debb901cbf3c95797cddab062a5
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-02 16:50:22 +0530

    Update notifications data

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,1 +1,10 @@
-export const notifications = [];
+export const notifications = [
+  {
+    "id": 1767352819972,
+    "title": {
+      "en": "Mega Dairy Inaguration at Kolagal Grama",
+      "ka": "ಕೊಳಗಲ್ ಗ್ರಾಮದಲ್ಲಿ ಮೆಗಾ ಡೈರಿ ಉದ್ಘಾಟನೆ"
+    },
+    "date": "1-1-26"
+  }
+];
