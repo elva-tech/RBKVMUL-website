e4a8657d5b68719250011db6923250e49fe97e4e
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-02 13:18:25 +0530

    Update news data

--- src/data/news.js
+++ src/data/news.js
@@ -1,1 +1,14 @@
-export const news = [];
+export const news = [
+  {
+    "id": 1767340102482,
+    "title": {
+      "en": "Foundation Stone Laying Ceremony of Mega Dairy",
+      "ka": "ಮೆಗಾ ಡೈರಿಯ ಭೂಮಿಪೂಜೆ ಸಮಾರಂಭ"
+    },
+    "description": {
+      "en": "The Honorable Chief Minister laid the foundation stone",
+      "ka": "ಮಾನ್ಯ ಮುಖ್ಯಮಂತ್ರಿಗಳು ಮೆಗಾ ಡೈರಿಯ ಭೂಮಿಪೂಜೆಯನ್ನು ನೆರವೇರಿಸಿದರು"
+    },
+    "image": "/images/1767340102479-1.jpg"
+  }
+];
