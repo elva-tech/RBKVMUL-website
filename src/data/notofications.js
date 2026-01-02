e4c95cb658e41fba770971aa828a5cccbeffd963
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-02 11:35:56 +0530

    Update notifications data

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,6 +1,14 @@
 export const notifications = [
   {
-    "id": 1767292792196,
+    "id": 1767333954577,
+    "title": {
+      "en": "Notification ",
+      "ka": "ನೋಟಿಫಿಕೇಶನ್ "
+    },
+    "date": "1-1-26"
+  },
+  {
+    "id": 1,
     "title": {
       "en": "Mega Dairy inaugurated at  Bellary Taluka Kolagal Grama",
       "ka": "ಬಳ್ಳಾರಿ ತಾಲ್ಲೂಕಿನ ಕೊಳಗಲ್ ಗ್ರಾಮದಲ್ಲಿ ಮೆಗಾ ಡೈರಿ ಉದ್ಘಾಟನೆ"
