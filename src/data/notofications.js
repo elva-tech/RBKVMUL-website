09447a561883d3a4bf7d7332e1f98435e84edf1e
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-02 13:10:57 +0530

    Update notifications data

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,1 +1,10 @@
-export const notifications = [];
+export const notifications = [
+  {
+    "id": 1767339655421,
+    "title": {
+      "en": "mega",
+      "ka": "ಆರ್‌ಬಿಕೆಮುಲ್"
+    },
+    "date": "1-1-26"
+  }
+];
