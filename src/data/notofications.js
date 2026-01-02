a9d3f93f0872f3626459d54e82e3a3fd7e3f8b0a
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-02 16:47:04 +0530

    Update notifications data

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,1 +1,10 @@
-export const notifications = [];
+export const notifications = [
+  {
+    "id": 1767352622798,
+    "title": {
+      "en": "Ega Dairy inauguration at ballari Taluka Kolagal Grama",
+      "ka": "ಬಳ್ಳಾರಿ ತಾಲೂಕಾ ಕೊಳಗಲ್ ಗ್ರಾಪಂನಲ್ಲಿ ಈಗಾ ಡೇರಿ ಉದ್ಘಾಟನೆ"
+    },
+    "date": "1-1-2026"
+  }
+];
