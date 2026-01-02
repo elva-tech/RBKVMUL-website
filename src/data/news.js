691f54f6f4fe63efb5beae477020f587222a5056
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-02 11:27:54 +0530

    Update news data

--- src/data/news.js
+++ src/data/news.js
@@ -1,29 +1,5 @@
 export const news = [
   {
-    "id": 1767292902032,
-    "title": {
-      "en": "Rbkvmul",
-      "ka": "ರವಿಕೆ ವಿ ಎಂ ಯು ಎಲ್ "
-    },
-    "description": {
-      "en": "Rbkvmul",
-      "ka": "ರವಿಕೆ ವಿ ಎಂ ಯು ಎಲ್ "
-    },
-    "image": "/images/1767292901955-1766996871169-01.jpeg"
-  },
-  {
-    "id": 1,
-    "title": {
-      "en": "Foundation Stone Laying Ceremony of Mega Dairy",
-      "ka": "ಮೆಗಾ ಡೈರಿಯ ಭೂಮಿಪೂಜೆ ಸಮಾರಂಭ"
-    },
-    "description": {
-      "en": "The Honorable Chief Minister laid the foundation stone...",
-      "ka": "ಮಾನ್ಯ ಮುಖ್ಯಮಂತ್ರಿಗಳು ಮೆಗಾ ಡೈರಿಯ ಭೂಮಿಪೂಜೆಯನ್ನು ನೆರವೇರಿಸಿದರು..."
-    },
-    "image": "/images/1.jpg"
-  },
-  {
     "id": 2,
     "title": {
       "en": "Foundation Stone Laying Ceremony of Mega Dairy",
