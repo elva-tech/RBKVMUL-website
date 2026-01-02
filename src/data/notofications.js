ffdd32fd20290f3f16969c6fa58bb90721ea1b77
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-02 12:39:37 +0530

    Update notifications data

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,1 +1,10 @@
-export const notifications = [];
+export const notifications = [
+  {
+    "id": 1767337775390,
+    "title": {
+      "en": "rbkvmul",
+      "ka": "ಆರ್‌ಬಿಕೆಮುಲ್"
+    },
+    "date": "1-1-26"
+  }
+];
