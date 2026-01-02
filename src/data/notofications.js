be703770611f36ed9c4f2bac24e5d2a6e0ba0687
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-02 12:24:53 +0530

    Update notifications data

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,1 +1,10 @@
-export const notifications = [];
+export const notifications = [
+  {
+    "id": 1767336892464,
+    "title": {
+      "en": "Mega Dairy inauguration at ballari Taluka Kolagal Grama",
+      "ka": "ಬಳ್ಳಾರಿ ತಾಲೂಕಾ ಕೊಳಗಲ್ ಗ್ರಾಪಂನಲ್ಲಿ ಮೆಗಾ ಡೇರಿ ಉದ್ಘಾಟನೆ"
+    },
+    "date": "1-1-26"
+  }
+];
