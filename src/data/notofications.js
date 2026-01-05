53140db92f78ac1539692192229c46e8032b224c
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-05 16:32:09 +0530

    Delete notification

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,14 +1,5 @@
 export const notifications = [
   {
-    "id": 1767610867650,
-    "title": {
-      "en": "tender",
-      "ka": "tender"
-    },
-    "date": "1-1-26",
-    "fileUrl": "/pdfs/notif-1767610865893-logo2.png"
-  },
-  {
     "id": 1767422647952,
     "title": {
       "en": "Vehicle Tender",
