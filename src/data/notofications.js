50cf1e9aec8b91d51d8908b6a4917932ca4c170c
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-05 17:28:50 +0530

    Delete notification

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,14 +1,5 @@
 export const notifications = [
   {
-    "id": 1767613677714,
-    "title": {
-      "en": "tender",
-      "ka": "tender"
-    },
-    "date": "1-1-26",
-    "fileUrl": "https://raw.githubusercontent.com/elva-tech/RBKVMUL-website/main/public/pdfs/notif-1767613676393-logo2.png"
-  },
-  {
     "id": 1767422647952,
     "title": {
       "en": "Vehicle Tender",
