87aec142b837446b26f57bbf056434a85da1e2b8
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-05 17:18:01 +0530

    Add notification with fixed URL

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,11 +1,20 @@
 export const notifications = [
   {
+    "id": 1767613677714,
+    "title": {
+      "en": "tender",
+      "ka": "tender"
+    },
+    "date": "1-1-26",
+    "fileUrl": "https://raw.githubusercontent.com/elva-tech/RBKVMUL-website/main/public/pdfs/notif-1767613676393-logo2.png"
+  },
+  {
     "id": 1767422647952,
     "title": {
       "en": "Vehicle Tender",
       "ka": "ವಾಹನ ಟೆಂಡರ್"
     },
     "date": "03/01/2026",
-    "fileUrl": "https://github.com/elva-tech/RBKVMUL-website/tree/main/public/pdfs" 
+    "fileUrl": "https://github.com/elva-tech/RBKVMUL-website/tree/main/public/pdfs"
   }
 ];
