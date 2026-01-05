2760cedd103fe4204e449dfb007c5cc174e17e51
vittesh B <vitheshb2003@gmail.com>
2026-01-05 18:15:42 +0530

    Remove notification entry from notifications.js

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,11 +1,2 @@
 export const notifications = [
-  {
-    "id": 1767422647952,
-    "title": {
-      "en": "Vehicle Tender",
-      "ka": "ವಾಹನ ಟೆಂಡರ್"
-    },
-    "date": "03/01/2026",
-    "fileUrl": "https://elva-tech.github.io/RBKVMUL-website/pdfs/notif-1767613676393-logo2.png"
-  }
 ];
