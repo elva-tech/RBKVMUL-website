d1979378e4cc457ee87badbf3cd24a0e19a9b983
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-05 23:53:28 +0530

    Add notification

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,1 +1,11 @@
-export const notifications = [];
+export const notifications = [
+  {
+    "id": 1767637404499,
+    "title": {
+      "en": "vehicle tender",
+      "ka": "vehicle tender"
+    },
+    "date": "2026-01-05",
+    "fileUrl": "https://github.com/elva-tech/RBKVMUL-website/tree/main/public/pdfs/notif-1767637402761-founder-of-milk-verghese-Kurien-Photo.png"
+  }
+];
