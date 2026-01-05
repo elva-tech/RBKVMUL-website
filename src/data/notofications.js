ff3be497fd61ba88f894e7f7cc13b3cb82297fd1
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-05 23:36:05 +0530

    Add notification

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,5 +1,14 @@
 export const notifications = [
   {
+    "id": 1767636360846,
+    "title": {
+      "en": "vehicle tender ",
+      "ka": "tender"
+    },
+    "date": "2026-01-05",
+    "fileUrl": "/pdfs/notif-1767636358965-logo2.png"
+  },
+  {
     "id": 1767619275329,
     "title": {
       "en": "tender",
