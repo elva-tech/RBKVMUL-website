21e2657fb3df8d70b8cf3018918a4cae22a54132
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-05 23:15:09 +0530

    Delete notification

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -7,14 +7,5 @@ export const notifications = [
     },
     "date": "2026-01-05",
     "file": "https://elva-tech.github.io/RBKVMUL-website/pdfs/notif-1767619273591-logo2.png"
-  },
-  {
-    "id": 1767422647952,
-    "title": {
-      "en": "Vehicle Tender",
-      "ka": "ವಾಹನ ಟೆಂಡರ್"
-    },
-    "date": "03/01/2026",
-    "fileUrl": "https://elva-tech.github.io/RBKVMUL-website/pdfs/notif-1767613676393-logo2.png"
   }
 ];
