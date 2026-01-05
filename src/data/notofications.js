78d4f46972f31b9fad189189e72023f903e403d2
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-06 00:10:46 +0530

    Delete notification

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,11 +1,1 @@
-export const notifications = [
-  {
-    "id": 1767637404499,
-    "title": {
-      "en": "vehicle tender",
-      "ka": "vehicle tender"
-    },
-    "date": "2026-01-05",
-    "fileUrl": "https://github.com/elva-tech/RBKVMUL-website/tree/main/public/pdfs/notif-1767637402761-founder-of-milk-verghese-Kurien-Photo.png"
-  }
-];
+export const notifications = [];
