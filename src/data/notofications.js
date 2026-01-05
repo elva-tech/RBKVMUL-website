1ca4fde33bce2a87a2710861bfd25d08cc63377b
vittesh B <vitheshb2003@gmail.com>
2026-01-05 18:14:19 +0530

    Add initial notifications data

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,1 +1,11 @@
-export const notifications = [];
+export const notifications = [
+  {
+    "id": 1767422647952,
+    "title": {
+      "en": "Vehicle Tender",
+      "ka": "ವಾಹನ ಟೆಂಡರ್"
+    },
+    "date": "03/01/2026",
+    "fileUrl": "https://elva-tech.github.io/RBKVMUL-website/pdfs/notif-1767613676393-logo2.png"
+  }
+];
