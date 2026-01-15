48e1d67e27f8207f9cd8fd89fc37a3f156cf4184
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-01-16 01:19:08 +0530

    Update announcement (Add/Remove images & text)

--- src/data/popupData.js
+++ src/data/popupData.js
@@ -13,9 +13,6 @@ export const popupData = {
     "ka": "ಬಳ್ಳಾರಿ ತಾಲೂಕು ಕೊಳಗಲ್ ಗಾರ್ಮಾದಲ್ಲಿ ಬರಲಿರುವ ಮೆಗಾ ಡೈರಿ. ಕರ್ನಾಟಕ ಗಣಿಗಾರಿಕೆ ಪರಿಸರ ಪುನಃಸ್ಥಾಪನೆ ನಿಗಮ (ಕೆಎಂಇಆರ್‌ಸಿ) ನಿಂದ ಅನುಮೋದನೆ."
   },
   "images": [
-    "/assets/popup-1767345167328-Announcement-image.png",
-    "/assets/popup-1768506147967-1767292901955-1766996871169-01.jpeg",
-    "/assets/popup-1768506150985-1767340102479-1.jpg",
-    "/assets/popup-1768506153955-1767340363805-2.jpg"
+    "/assets/popup-1767345167328-Announcement-image.png"
   ]
 };
