ec2387311f36182e3049234f4ddd55e93b7a7c91
Sai Krishna E L <42062155+saikrishna96111@users.noreply.github.com>
2026-07-22 15:18:46 +0530

    Add notification

--- src/data/notofications.js
+++ src/data/notofications.js
@@ -1,5 +1,14 @@
 export const notifications = [
   {
+    "id": 1784713724613,
+    "title": {
+      "en": "The development of the dairy sector in the region was formally initiated in 1989 with the establishment of the Raichur & Bellary Co-operative Milk Producers’ Societies Union Ltd. during 1989–90. The Union was formed to strengthen organized milk production and marketing by bringing rural milk producers under a structured co-operative framework. From its inception, the Union aimed to create a sustainable system that would benefit farmers economically while ensuring a steady supply of quality milk to consumers.",
+      "ka": "1989-90ರ ಅವಧಿಯಲ್ಲಿ ರಾಯಚೂರು ಮತ್ತು ಬಳ್ಳಾರಿ ಸಹಕಾರಿ ಹಾಲು ಉತ್ಪಾದಕರ ಸಂಘಗಳ ಒಕ್ಕೂಟ ನಿಯಮಿತ ಸ್ಥಾಪನೆಯೊಂದಿಗೆ ಈ ಪ್ರದೇಶದಲ್ಲಿ ಡೈರಿ ಕ್ಷೇತ್ರದ ಅಭಿವೃದ್ಧಿಯು ಔಪಚಾರಿಕವಾಗಿ ಪ್ರಾರಂಭವಾಯಿತು. ಗ್ರಾಮೀಣ ಹಾಲು ಉತ್ಪಾದಕರನ್ನು ರಚನಾತ್ಮಕ ಸಹಕಾರಿ ಚೌಕಟ್ಟಿನ ಅಡಿಯಲ್ಲಿ ತರುವ ಮೂಲಕ ಸಂಘಟಿತ ಹಾಲು ಉತ್ಪಾದನೆ ಮತ್ತು ಮಾರುಕಟ್ಟೆಯನ್ನು ಬಲಪಡಿಸಲು ಈ ಒಕ್ಕೂಟವನ್ನು ರಚಿಸಲಾಯಿತು. ಪ್ರಾರಂಭದಿಂದಲೂ, ಗ್ರಾಹಕರಿಗೆ ಗುಣಮಟ್ಟದ ಹಾಲಿನ ಸ್ಥಿರ ಪೂರೈಕೆಯನ್ನು ಖಚಿತಪಡಿಸಿಕೊಳ್ಳುವ ಜೊತೆಗೆ ರೈತರಿಗೆ ಆರ್ಥಿಕವಾಗಿ ಪ್ರಯೋಜನವನ್ನು ನೀಡುವ ಸುಸ್ಥಿರ ವ್ಯವಸ್ಥೆಯನ್ನು ರಚಿಸುವ ಗುರಿಯನ್ನು ಒಕ್ಕೂಟ ಹೊಂದಿದೆ."
+    },
+    "date": "2026-07-22",
+    "fileUrl": "https://raw.githubusercontent.com/elva-tech/RBKVMUL-website/main/public/pdfs/notif-1784713722072-Rbkvmul-notification.pdf"
+  },
+  {
     "id": 1768506834196,
     "title": {
       "en": "milk union",
